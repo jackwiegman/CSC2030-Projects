40d32aefb2aafb35d62191788e4b5604c4b47062
Jack Wiegman <33030441+jackwiegman@users.noreply.github.com>
2025-01-23 02:04:12 -0700

    Make multiply

---
+++ multiply/Multiply.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int multiply(int num1, int num2) {
+    int sum = 0;
+    for (int i = 0; i < num1; i++) {
+        sum += num2;
+    }
+    return sum;
+}
+
+int main() {
+    int num1 = 4;
+    int num2 = 8;
+    int product = multiply(num1, num2);
+    printf("%d\n",product);
+    return 1;
+}
