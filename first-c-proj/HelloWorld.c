84e2b0098a9044b14fc381614d4a6fa2225fc642
Jack Wiegman <33030441+jackwiegman@users.noreply.github.com>
2025-01-24 23:48:36 -0700

    format

--- first-c-proj/HelloWorld.c
+++ first-c-proj/HelloWorld.c
@@ -1,8 +1,26 @@
+/**
+ * @file
+ * @brief
+ */
+
 #include <stdio.h>
 
+/**
+ * @brief
+ *
+ * @param num1
+ * @param num2
+ * @return
+ */
 int doMath(int num1, int num2) { return num1 + num2; }
 
+/**
+ * @brief
+ *
+ * @return
+ */
 int main() {
+
     printf("Hello, World!\n");
     return 0;
 }
--- multiply/Multiply.c
+++ multiply/Multiply.c
@@ -10,8 +10,9 @@ int multiply(int num1, int num2) {
 
 int main() {
     int num1 = 4;
+
     int num2 = 8;
     int product = multiply(num1, num2);
-    printf("%d\n",product);
+    printf("%d\n", product);
     return 1;
 }
