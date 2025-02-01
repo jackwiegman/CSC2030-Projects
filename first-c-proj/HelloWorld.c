2bfc93374d9ba69793a4d604123f3b63f8d0f199
Jack Wiegman <33030441+jackwiegman@users.noreply.github.com>
2025-01-31 17:48:49 -0700

    vscode stuff

--- first-c-proj/HelloWorld.c
+++ first-c-proj/HelloWorld.c
@@ -1,26 +1,9 @@
 /**
- * @file
- * @brief
+ * @file HelloWorld.c
  */
-
 #include <stdio.h>
 
-/**
- * @brief
- *
- * @param num1
- * @param num2
- * @return
- */
-int doMath(int num1, int num2) { return num1 + num2; }
-
-/**
- * @brief
- *
- * @return
- */
 int main() {
-
     printf("Hello, World!\n");
     return 0;
 }
