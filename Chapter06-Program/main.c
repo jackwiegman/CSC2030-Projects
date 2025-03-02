6e6b90ec80cbb280b3fa000f3cd595acafdc3f38
Jack Wiegman <33030441+jackwiegman@users.noreply.github.com>
2025-03-01 19:18:36 -0700

    definitely didn't delete the program on accident and need to rewrite it
    more

---
+++ Chapter06-Program/main.c
@@ -0,0 +1,173 @@
+/**
+ * @file main.c
+ * @brief Multiply matrices.
+ */
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+_Bool continueProgram();
+int getRows();
+int getCols();
+void populateMatrix(int rows, int cols, int matrix[rows][cols]);
+void printMatrix(int rows, int cols, int matrix[rows][cols]);
+void multiplyMatrix(int rows1, int cols1, int matrix1[rows1][cols1], int rows2, int cols2,
+                    int matrix2[rows2][cols2], int rowsP, int colsP, int product[rowsP][colsP]);
+
+const int MAX_MATRIX_VALUE = 99;
+const int MAX_MATRIX_DEMENSION = 50;
+int main() {
+
+    int matrix1Rows, matrix1Cols, matrix2Rows, matrix2Cols;
+    int matrix1[matrix1Rows][matrix1Cols];
+    int matrix2[matrix2Rows][matrix2Cols];
+
+    while (1) {
+
+        puts("This program will multiply two matrices: ");
+
+        if (!continueProgram()) {
+            printf("%s", "Thank you, come again!");
+            return 0;
+        }
+
+        puts("Please enter the information for the first matrix.");
+        puts("Keep in mind, in order to multiply matrices, the column deminsion of the first "
+             "matrix must match the row deminsion of the second.");
+        matrix1Rows = getRows();
+        matrix1Cols = getCols();
+        matrix2Rows = matrix1Cols;
+
+        int matrix1[matrix1Rows][matrix1Cols];
+
+        /* Populate and print first matrix */
+        populateMatrix(matrix1Rows, matrix1Cols, matrix1);
+        printMatrix(matrix1Rows, matrix1Cols, matrix1);
+
+        printf("%s%d%s", "The second matrix will have ", matrix1Cols, " rows.");
+        matrix2Cols = getCols();
+        int matrixPRows = matrix1Rows;
+        int matrixPCols = matrix2Cols;
+
+        int matrix2[matrix2Rows][matrix2Cols];
+        int productMatrix[matrix1Rows][matrix2Cols];
+
+        populateMatrix(matrix2Rows, matrix2Cols, matrix2);
+        printMatrix(matrix2Rows, matrix2Cols, matrix2);
+
+        multiplyMatrix(matrix1Rows, matrix1Cols, matrix1, matrix2Rows, matrix2Cols, matrix2,
+                       matrixPRows, matrixPCols, productMatrix);
+
+        puts("The product of the two matrices:");
+        printMatrix(matrixPRows, matrixPCols, productMatrix);
+    }
+    return 0;
+}
+
+_Bool continueProgram() {}
+
+void multiplyMatrix(int rows1, int cols1, int matrix1[rows1][cols1], int rows2, int cols2,
+                    int matrix2[rows2][cols2], int rowsP, int colsP, int product[rowsP][colsP]) {
+
+    int sum = 0;
+
+    for (int i = 0; i < rows1; i++) {
+        for (int j = 0; j < cols2; j++) {
+            for (int k = 0; k < rows2; k++) {
+                // Multiply corresponding places in arrays
+                sum += matrix1[i][k] * matrix2[j][k];
+            }
+            product[i][j] = sum; // Put the final sum of products into result array
+            sum = 0;             // Reset sum
+        }
+    }
+}
+
+/**
+ * @brief Print a matrix
+ *
+ * @param rows      Number of rows in matrix
+ * @param cols      Number of columns in matrix
+ * @param matrix    2D array of ints with deminsions [rows][cols]
+ * @return void
+ */
+void printMatrix(int rows, int cols, int matrix[rows][cols]) {
+
+    printf("%s", "{ ");
+    for (int i = 0; i < rows; i++) {
+        printf("%s", "{ ");
+
+        for (int j = 0; j < cols; j++) {
+            printf("%d%s", matrix[i][j], ", ");
+        }
+
+        printf("%s", " }");
+    }
+    printf("%s", " }");
+}
+
+/**
+ * @brief Populate matrix with random integers from 0 to \ref MAX_MATRIX_DEMENSION
+ * Take in a matrix and the deminsions of said matrix. Loop through all elements in matrix and
+ * generate a random integer to set the element.
+ *
+ * @param rows      Number of rows in matrix
+ * @param cols      Number of columns in matrix
+ * @param matrix    2D array of ints with deminsions [rows][cols]
+ * @return Matrix populated with random integers
+ */
+void populateMatrix(int rows, int cols, int matrix[rows][cols]) {
+    int randInt;
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            randInt = rand() % MAX_MATRIX_VALUE;
+            matrix[i][j] = randInt;
+        }
+    }
+}
+
+/**
+ * @brief Get matrix row deminsions from user.
+ *
+ * @return int Number of rows from user
+ */
+int getRows() {
+    int input;
+
+    while (1) {
+        printf("%s%d%s", "Please enter the number of rows for the matrix from 1 to ",
+               MAX_MATRIX_DEMENSION, " or -1 to eixt.");
+        scanf("%d", &input);
+
+        if (-1 == input) {
+            return -1;
+        } else if (0 < input && input <= MAX_MATRIX_DEMENSION) {
+            return input;
+        }
+        puts("Input invalid. Try again.");
+    }
+    return MAX_MATRIX_DEMENSION;
+}
+
+/**
+ * @brief Get matrix column deminsions from user.
+ *
+ * @return int Number of columns from user
+ */
+int getCols() {
+    int input;
+
+    while (1) {
+        printf("%s%d%s", "Please enter the number of columns for the matrix(1-",
+               MAX_MATRIX_DEMENSION, "): ");
+        scanf("%d", &input);
+
+        if (0 < input && input <= MAX_MATRIX_DEMENSION) {
+            return input;
+        }
+        puts("Input invalid. Try again.");
+    }
+    return MAX_MATRIX_DEMENSION;
+}
