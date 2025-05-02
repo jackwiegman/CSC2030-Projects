0078eb69fd8c48478c8a402a2290a0e334a99aab
Jack Wiegman <33030441+jackwiegman@users.noreply.github.com>
2025-05-01 18:50:13 -0600

    Start chapter 8 program.

---
+++ Chapter08/ConcatenateStrings.c
@@ -0,0 +1,16 @@
+/**
+ * @file ConcatenateStrings.c
+ * @brief Concatenate two strings provided by a user.
+ * Prompt user at the terminal to enter two strings, entered one at a time. Then print the words,
+ * both before and after concatenating them, as well as the length of the concatenated word.
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    
+    strcat(char *, const char *)
+
+}
